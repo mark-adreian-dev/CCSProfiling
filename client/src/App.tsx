38be8a03d80707c28dbb036e0fc664afa758016e
mark-adreian-dev <markadreianramos@gmail.com>
2026-03-09 22:42:20 +0800

    CCSPS-15 | 09/03/2026 - Setting up dependency environment for AMI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,14 @@
-import { AppSidebar } from "@/core/presentation/components/base/app-sidebar"
-import { ChartAreaInteractive } from "@/core/presentation/components/base/chart-area-interactive"
-import { DataTable } from "@/core/presentation/components/base/data-table"
-import { SectionCards } from "@/core/presentation/components/base/section-cards"
-import { SiteHeader } from "@/core/presentation/components/base/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/core/presentation/components/base/ui/sidebar"
-import data from "@/app/dashboard/data.json"
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./core/routes/Router";
 
 export function App() {
-  return <SidebarProvider
-    style={
-      {
-        "--sidebar-width": "calc(var(--spacing) * 72)",
-        "--header-height": "calc(var(--spacing) * 12)",
-      } as React.CSSProperties
-    }
-  >
-    <AppSidebar variant="inset" />
-    <SidebarInset>
-      <SiteHeader />\
-      <div className="flex flex-1 flex-col">
-        <div className="@container/main flex flex-1 flex-col gap-2">
-          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            <SectionCards />
-            <div className="px-4 lg:px-6">
-              <ChartAreaInteractive />
-            </div>
-            <DataTable data={data} />
-          </div>
-        </div>
-      </div>
-    </SidebarInset>
-  </SidebarProvider>;
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/core/config/toaster.config.ts
@@ -0,0 +1,3 @@
+export const TOASTER_CONFIG = {
+  AUTH: "auth-toaster",
+};
